332e5782a7bc4abded9d2f02f4119191df21be43
Eva Bebina <bebinaeva@gmail.com>
2021-02-03 23:13:38 +0300

    with functions

---
+++ Rock_paper2.py
@@ -0,0 +1,36 @@
+import random
+from enum import IntEnum
+
+class Action(IntEnum):
+    Rock = 0
+    Paper = 1
+    Scissors = 2
+def get_user_selection():
+    user_input = input("Enter a choice (rock[0], paper[1], scissors[2]): ")
+    selection = int(user_input)
+    action = Action(selection)
+    return action
+def get_user_selection():
+    choices = [f"{action.name}[{action.value}]" for action in Action]
+    choices_str = ", ".join(choices)
+    selection = int(input(f"Enter a choice ({choices_str}): "))
+    action = Action(selection)
+    return action
+def determine_winner(user_action, computer_action):
+    if user_action == computer_action:
+        print(f"Both players selected {user_action.name}. It's a tie!")
+    elif user_action == Action.Rock:
+        if computer_action == Action.Scissors:
+            print("Rock smashes scissors! You win!")
+        else:
+            print("Paper covers rock! You lose.")
+    elif user_action == Action.Paper:
+        if computer_action == Action.Rock:
+            print("Paper covers rock! You win!")
+        else:
+            print("Scissors cuts paper! You lose.")
+    elif user_action == Action.Scissors:
+        if computer_action == Action.Paper:
+            print("Scissors cuts paper! You win!")
+        else:
+            print("Rock smashes scissors! You lose.")
